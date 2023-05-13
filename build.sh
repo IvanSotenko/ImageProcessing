4423e58e001a8d803509fbba6bf7f534e24a220e
IvanSotenko <eevandeya@gmail.com>
2023-05-13 19:33:09 +0300

    Change build.sh to make it work

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -o pipefail
 echo "Restoring dotnet tools..."
 dotnet tool restore
 
-PATH_TO_THE_TEST_IMAGES_FOLDER=${PATH_TO_THE_TEST_IMAGES_FOLDER-../../../../../../../ImageProcessing/testImages}
+PATH_TO_THE_TEST_IMAGES_FOLDER=../../../../../../../ImageProcessing/testImages
 
-PATH_TO_THE_TEST_IMAGES_FOLDER=$PATH_TO_THE_TEST_IMAGES_FOLDER;FAKE_DETAILED_ERRORS=true dotnet run --project ./build/build.fsproj -- -t "$@"
+FAKE_DETAILED_ERRORS=true PATH_TO_THE_TEST_IMAGES_FOLDER=$PATH_TO_THE_TEST_IMAGES_FOLDER dotnet run --project ./build/build.fsproj -- -t "$@"
